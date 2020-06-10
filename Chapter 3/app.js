b48f2a91d1cfa498c0f33380c9758817b280b973
Monalisa04 <monalisa.svnit@gmail.com>
2020-06-11 00:00:00 +0100

    Checking in routes module

--- Chapter 3/app.js
+++ Chapter 3/app.js
@@ -1,36 +1,5 @@
 const http = require("http");
-const fs = require("fs");
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  const body = [];
+const routes = require("./routes.js");
 
-  if (req.url === "/") {
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"></input></form></body>'
-    );
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    req.on("data", (chunk) => {
-      body.push(chunk);
-      console.log(body);
-    });
-    req.on("end", () => {
-      const parsedBuffer = Buffer.concat(body).toString();
-      console.log("parsedBuffer", parsedBuffer);
-      const msg = parsedBuffer.split("=")[1];
-      console.log("msg", msg);
-      fs.writeFileSync("message.txt", msg);
-    });
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<head></head>");
-  res.write("<title>My nodejs page</title>");
-  res.write("<body>Hello from node js</body>");
-  res.end();
-});
+const server = http.createServer(routes);
 server.listen(3000);
